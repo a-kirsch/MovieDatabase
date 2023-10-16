32a01c526e6339d4820cd5c9818fe53612babc61
a-kirsch <a.kirsch@wustl.edu>
2023-10-16 16:07:29 -0500

    Initial Commit

---
+++ AdamKirsch-Lab4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdamKirsch-Lab4
+//
+//  Created by Adam Kirsch on 10/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AdamKirsch-Lab4UITests/AdamKirsch_Lab4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AdamKirsch_Lab4UITestsLaunchTests.swift
+//  AdamKirsch-Lab4UITests
+//
+//  Created by Adam Kirsch on 10/16/23.
+//
+
+import XCTest
+
+final class AdamKirsch_Lab4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
